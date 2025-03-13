6bedc143aade208ec3e34caae288368c3b29a111
Nazarii Arshynskyi <na@ggr.group>
2025-03-13 21:49:29 +0000

    added logo

--- src/bank_system/view/BankUI.java
+++ src/bank_system/view/BankUI.java
@@ -28,7 +28,13 @@ public class BankUI implements UI{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(new GridBagLayout());
+        // frame.getContentPane().setBackground(Color.LIGHT_GRAY);
         
+        // Add Image (logo)
+        ImageIcon logoIcon = new ImageIcon(getClass().getResource("/resources/logo.png"));
+        Image logoImage = logoIcon.getImage().getScaledInstance(200, 150, Image.SCALE_SMOOTH);
+        JLabel logoLabel = new JLabel(new ImageIcon(logoImage));
+
         // Title
         JLabel titleLabel = new JLabel("Welcome, " + this.user.getUsername(), JLabel.CENTER);
         titleLabel.setFont(new Font("Serif", Font.BOLD, 24));
@@ -51,57 +57,65 @@ public class BankUI implements UI{
         
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
-
-        // Title Label
+        
+        // Logo
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 3;
         gbc.anchor = GridBagConstraints.CENTER;
+        frame.add(logoLabel, gbc);
+
+        // Title Label
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 3;
+        gbc.anchor = GridBagConstraints.NORTH;
         frame.add(titleLabel, gbc);
         
         // Balance Label
         gbc.gridx = 0;
-        gbc.gridy = 1;
+        gbc.gridy = 2;
         gbc.anchor = GridBagConstraints.NORTH;
         frame.add(balanceLabel, gbc);
         
         // User id
         gbc.gridx = 0;
-        gbc.gridy = 2;
+        gbc.gridy = 3;
         gbc.anchor = GridBagConstraints.NORTH;
         frame.add(userIdLabel, gbc);
         
         
+
         gbc.insets = new Insets(0, 0, 0, 0);
         gbc.anchor = GridBagConstraints.CENTER;
         gbc.gridwidth = 1;
         gbc.gridx = 0;
-        gbc.gridy = 3;
+        gbc.gridy = 4;
         gbc.ipady = 20;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         frame.add(depositButton, gbc);
 
         gbc.gridx = 1;
-        gbc.gridy = 3;
+        gbc.gridy = 4;
         gbc.ipady = 20;
         gbc.fill = GridBagConstraints.REMAINDER;
         frame.add(withdrawButton, gbc);
 
         gbc.gridx = 0;
-        gbc.gridy = 4;
+        gbc.gridy = 5;
         gbc.ipady = 20;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         frame.add(historyButton, gbc);
         
         gbc.gridx = 1;
-        gbc.gridy = 4;
+        gbc.gridy = 5;
         gbc.ipady = 20;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         frame.add(transferButton, gbc);
 
         // LogOut Button
         gbc.gridx = 0;
-        gbc.gridy = 5;
+        gbc.gridy = 6;
         gbc.gridwidth = 2;
         gbc.ipady = 20;
         gbc.anchor = GridBagConstraints.CENTER;
